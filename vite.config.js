161da9793d13b395104bb41666b60e41f9866d92
Edmund Blackadder <vissak2016@gmail.com>
2025-03-24 01:19:46 +0530

    Update base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Vouchers', // Matches the repository name
+  base: '/', // Matches the repository name
   css: {
     postcss: './postcss.config.cjs',
   },
